428d6663d0b1b8669f880829472996fd5ea80b2f
BDunnCode <BDunnCode@github.com>
2023-11-02 15:11:13 -0500

    fixed some bugs

--- script.js
+++ script.js
@@ -28,24 +28,25 @@ class Calculator {
     }
 
     updateDisplay() {
-        this.outputRow2TextElement.innerText = this.this.outputRow2TextElement
+        this.outputRow2TextElement.innerText = this.outputRow2
     }
 
 }
 
 
-const numberButtons = document.querySelectorAll(['data-number'])
-const deleteButton = document.querySelector(['data-delete'])
+const numberButtons = document.querySelectorAll('[data-number]')
+const deleteButton = document.querySelector('[data-delete]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
-const allClearButton = document.querySelector(['data-allclear'])
-const outputRow1TextElement = document.querySelector(['data-outputrow1'])
-const outputRow2TextElement = document.querySelector(['data-outputrow2'])
+const allClearButton = document.querySelector('[data-allclear]')
+const outputRow1TextElement = document.querySelector('[data-outputrow1]')
+const outputRow2TextElement = document.querySelector('[data-outputrow2]')
 
 const calculator = new Calculator(outputRow1TextElement, outputRow2TextElement )
 
 numberButtons.forEach(button => { 
     button.addEventListener('click', () => {
+        console.log('hi')
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
